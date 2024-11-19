3b6ac5ded3aa1ad13b59ab828ce76cb08b36a039
Morten Svendgård <mortensvendgaard@gmail.com>
2024-11-19 23:29:26 +0100

    Soft min second order and env update

---
+++ cbf_tools.py
@@ -0,0 +1,53 @@
+import numpy as np
+import scipy as sp
+from abc import ABC, abstractmethod
+
+def single_exponential_cbf_solver(u_ref: np.ndarray, Lg_psi: np.ndarray, Lf_psi: float, psi: float, p1: float) -> np.ndarray:
+
+    alpha = Lg_psi
+    beta = - Lf_psi - p1 * psi
+
+    a_len_squared = np.sum(alpha ** 2)
+
+    # If a_len_squared is (almost) zero, then return u_ref
+    if a_len_squared < 1e-6:
+        return u_ref
+    
+    # Else do the derived solution
+    return max(beta - np.dot(alpha, u_ref), 0) * alpha / a_len_squared + u_ref
+
+class FirstOrderGeneralLie(ABC):
+
+    @abstractmethod
+    def get_Lg_Lf_and_psi(self, states: dict[str, np.ndarray]) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        pass
+
+
+class SoftMinLie(FirstOrderGeneralLie):
+
+    def __init__(self, first_order_cbfs: list[FirstOrderGeneralLie], k: float):
+        self.first_order_cbfs = first_order_cbfs
+        self.k = k
+
+    def get_Lg_Lf_and_psi(self, states: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        original_Lg_psis, original_Lf_psis, original_psis = zip(*[cbf.get_Lg_Lf_and_psi(states) for cbf in self.first_order_cbfs])
+        # Concatenate all CBF components
+        Lg_psis = np.concatenate([original_Lg_psi for original_Lg_psi in original_Lg_psis], axis=0)
+        Lf_psis = np.concatenate([original_Lf_psi for original_Lf_psi in original_Lf_psis], axis=0)
+        psis = np.concatenate([original_psi for original_psi in original_psis], axis=0)
+
+        #return Lg_psis, - Lf_psis - self.p1 * psis
+
+        # Compute the softmin as defined in the paper
+        ins = -self.k * psis
+        logsumexp_val = sp.special.logsumexp(ins)
+        combined_psi = - logsumexp_val / self.k
+
+        # Correct coefficient calculation using logsumexp for numerical stability
+        coeffs = np.exp(ins - logsumexp_val)
+
+        # Compute combined Lf and Lg
+        combined_Lf_psi = np.sum(coeffs * Lf_psis, axis=0)
+        combined_Lg_psi = np.sum(coeffs.reshape(-1, 1) * Lg_psis, axis=0)
+
+        return combined_Lg_psi, combined_Lf_psi, combined_psi
---
+++ soft_min_nav_linear.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+from cbf_tools import FirstOrderGeneralLie, SoftMinLie, single_exponential_cbf_solver
+from navigationgym import DotDynamicsNormal, runner
+
+class MultiObjectCBF2Order(FirstOrderGeneralLie):
+
+    def __init__(self, radius: float, u_max: float, p1: float):
+        self.radius = radius
+        self.u_max = u_max
+        self.p1 = p1
+    
+    def get_Lg_Lf_and_psi(self, observation: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+        epsilon = 0
+        pos_vector = 0
+        vel_vector = observation[3:5]
+        circle_centers = observation[5:].reshape(-1, 2)
+
+        #print((pos_vector - circle_centers).shape)
+        h = np.sum((pos_vector - circle_centers) ** 2, axis=1) - self.radius ** 2 - epsilon
+        h_dot = 2*np.sum((pos_vector - circle_centers) * vel_vector, axis=1)
+
+        psi = h_dot + self.p1 * h
+        Lf_psi = 2*np.dot(vel_vector, vel_vector) + self.p1*h_dot
+        Lg_psi = 2*(pos_vector - circle_centers)
+
+        return Lg_psi, Lf_psi, psi
+    
+
+class DotDynamicsNormalSoftMin(DotDynamicsNormal):
+
+    def __init__(self, dt: float, radius: float, u_max: float, p1: float, p2: float):
+        super().__init__(dt=dt)
+        self.soft_min_cbf = SoftMinLie([MultiObjectCBF2Order(radius, u_max, p1)], k=5)
+        self.radius = radius
+        self.u_max = u_max
+        self.p2 = p2
+
+    def perform_step(self, u: np.ndarray, observation: np.ndarray) -> np.ndarray:
+
+        # Get constraints
+        Lg_psi, Lf_psi, psi = self.soft_min_cbf.get_Lg_Lf_and_psi(observation)
+
+        # Calculate optimal control
+        u_safe = single_exponential_cbf_solver(u_ref=u, Lg_psi=Lg_psi, Lf_psi=Lf_psi, psi=psi, p1=self.p2)
+
+        return super().perform_step(u_safe, observation)
+    
+if __name__ == "__main__":
+    
+    U_MAX = 50
+     
+    dynamics = DotDynamicsNormalSoftMin(dt=1e-2, radius=10, u_max=U_MAX, p1=2, p2=1)
+
+    # Run the simulation
+    runner(
+         dynamics=dynamics,
+         lidar_distance=130,
+         lidar_num=32,
+         u_max=U_MAX,
+         render=True
+    )
