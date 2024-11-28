b4710d56c3340d5bc7ffd3059a01c3c22e8e7a7a
Morten Svendgård <mortensvendgaard@gmail.com>
2024-11-28 03:10:09 +0100

    Fast tests

--- test_through.py
+++ test_through.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     #results_base.mkdir(exist_ok=True)
 
     for track, results_name, num_steps in [('worlds/tight_track.json', 'tight_track', 1000),
-                                ('worlds/random_track_sparse.json', 'random_track_sparse', 1300),
                                 ('worlds/random_track_quadrant.json', 'random_track_quadrant', 2000),
                                 ('worlds/zigzag_tight_track.json', 'zigzag_tight_track', 2300)]:
         
@@ -47,3 +46,35 @@ if __name__ == "__main__":
             results_path=results_base / Path(results_name+"_soft"),
             world_file=Path(track)
         )
+
+    track, results_name, num_steps = ('worlds/random_track_sparse.json', 'random_track_sparse', 1300)
+
+    dynamics_tri = DotDynamicsNormalTrianglesCBF(dt=1e-2, p1=50, p2=50, control_size=U_MAX,
+                                                initial_state=np.array([65.0, 65.0, 0.0, 0.0, 0.0]),
+                                                constant_control=np.array([U_MAX, U_MAX]))
+    
+    dynamics_soft = DotDynamicsNormalSoftMin(dt=1e-2, radius=10, u_max=U_MAX, p1=50, p2=50,
+                                                initial_state=np.array([65.0, 65.0, 0.0, 0.0, 0.0]),
+                                                constant_control=np.array([U_MAX, U_MAX]))
+    
+    runner(
+            dynamics=dynamics_tri,
+            lidar_distance=130,
+            lidar_num=32,
+            u_max=U_MAX,
+            render=True,
+            num_steps=num_steps,
+            results_path=results_base / Path(results_name+"_triangles"),
+            world_file=Path(track)
+        )
+
+    runner(
+        dynamics=dynamics_soft,
+        lidar_distance=130,
+        lidar_num=32,
+        u_max=U_MAX,
+        render=True,
+        num_steps=num_steps,
+        results_path=results_base / Path(results_name+"_soft"),
+        world_file=Path(track)
+    )
